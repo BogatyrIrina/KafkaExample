5e93765b0641511a2512eb509af0a7dbfa385941
admin <kochereva_irina@mail.ru>
2024-05-11 13:54:40 +0300

    Initial commit

---
+++ KafkaExample/src/main/java/com/example/KafkaExample/model/Foo1.java
@@ -0,0 +1,13 @@
+package com.example.KafkaExample.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Foo1 {
+
+    private String foo;
+}
